751e1f78ae8ab6805ef32045f995c3755cd76b4d
KovacsLevente199 <kovacslevente199@gmail.com>
2024-04-02 10:23:22 +0200

    -Adatbáziskezelő osztály hozzádva

---
+++ Redmine/Database/DatabaseQuery.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+
+namespace Redmine.Database
+{
+    public class DatabaseQuery
+    {
+        public DatabaseQuery(string CommandText) 
+        {
+            string ConnectionCredentials = "";
+
+            using(SqlConnection connection = new SqlConnection(ConnectionCredentials)) 
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand query = new SqlCommand(CommandText, connection);
+                    SqlDataReader reader = query.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        SortedDictionary<string, object> row = new SortedDictionary<string, object>();   
+                        for(int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[reader.GetName(i)] = reader.GetValue(i);
+                        }
+
+                        rows.Add(row);
+                    }
+                    connection.Close();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.ToString());   
+                }
+            }
+        }
+
+        private List<SortedDictionary<string, object>> rows = new List<SortedDictionary<string, object>>();
+    }
+}
