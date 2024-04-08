d72bf75143abd8a240d6b96316294b0ccf10abbf
KovacsLevente199 <kovacslevente199@gmail.com>
2024-04-08 20:27:20 +0200

    -Új kliensoldali kód

--- Redmine/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Redmine.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
