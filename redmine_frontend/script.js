d74f8ee2eacfd0138c41f80b70895661b4090e8a
KovacsLevente199 <kovacslevente199@gmail.com>
2024-04-17 20:37:42 +0200

    -Feladat hozzáadás gomb megirva

--- redmine_frontend/script.js
+++ redmine_frontend/script.js
@@ -1,10 +1,6 @@
 function populateProjectList(data) {
     const container = document.getElementById('ProjectList');
-
     container.innerHTML = '';
-
-    console.log(data)
-
     data.forEach(item => {
         createProjectContainer(item,container);
     });
@@ -12,58 +8,163 @@ function populateProjectList(data) {
 
 function createProjectContainer(projectRecord,location)
 {
+
     let projectContainer = document.createElement('div');
-    projectContainer.classList.add('projectContainer');
+    projectContainer.classList.add('project-container');
+    projectContainer.setAttribute('data-project-type', projectRecord["typeID"]);
+    projectContainer.setAttribute('data-project-id', projectRecord["id"]);
+
+    let projectDetails = document.createElement('div');
+    projectDetails.classList.add('project-details');
 
     let projectTitlebar = document.createElement('div');
-    projectTitlebar.classList.add('projectTitlebar');
+    projectTitlebar.classList.add('project-title');
     projectTitlebar.textContent = projectRecord["name"];
 
     let projectContentArea = document.createElement('div');
-    projectContentArea.classList.add('projectContentArea');
+    projectContentArea.classList.add('project-content-area');
     projectContentArea.textContent = projectRecord["description"];
+    
 
     let projectfooter = document.createElement('div');
-    projectfooter.classList.add('projectfooter');
-    projectfooter.textContent = "Feladat hozzáadása"
+    projectfooter.classList.add('project-footer');
 
-    projectContainer.appendChild(projectTitlebar);
-    projectContainer.appendChild(projectContentArea);
-    projectContainer.appendChild(projectfooter);
+    let addTaskBtn = document.createElement('button');
+    addTaskBtn.classList.add('add-task-btn');
+    addTaskBtn.textContent = "Feladat hozzáadása"
+    addTaskBtn.addEventListener("click", (e) => {
+        console.log(e.target)
+        const parentWrapper = e.target.parentNode.parentNode.parentNode;
+        const createTaskWrapper = parentWrapper.querySelector(".create-task-wrapper");
+        if (createTaskWrapper.style.display === "none") {
+            createTaskWrapper.style.display = "flex"
+        } else {
+             createTaskWrapper.style.display = "none"
+        }
+        
+
+    })
+
+    projectfooter.appendChild(addTaskBtn)
+
+    
+
+    projectDetails.appendChild(projectTitlebar);
+    projectDetails.appendChild(projectContentArea);
+    projectDetails.appendChild(projectfooter);
+    projectContainer.appendChild(projectDetails);
+    createTaskAddLayout(projectContainer)
     
     location.appendChild(projectContainer);
 }
 
 function createTaskAddLayout(appendTo)
 {
-    console.log("lefut")
     let mainContainer = document.createElement('div');
-    mainContainer.classList.add('taskAddContainer')
-
-    let title = document.createElement('h1');
-    title.textContent = "Feladat létrehozása";
+    mainContainer.classList.add('create-task-wrapper')
+    mainContainer.style.display = "none"
 
     let name = document.createElement('div');
     name.textContent ="Név";
     let nameInput = document.createElement('input');
+    nameInput.classList.add('name-input')
 
     let description = document.createElement('div');
     description.textContent = "Leírás"
     let descriptionInput = document.createElement('input');
+    descriptionInput.classList.add('description-input')
+
+    let time = document.createElement('div');
+    time.textContent ="Határidő";
+    let timeInput = document.createElement('input');
+    timeInput.classList.add('time-input')
+    timeInput.type = "date";
 
     let developer = document.createElement('div');
     developer.textContent = "Fejlesztő";
-
     let developerSelect = document.createElement('select');
+    developerSelect.classList.add('developer-input')
 
-    mainContainer.appendChild(title);
+    fetch("https://localhost:7295/RedMineDataList/listdevelopers", {
+        method: "POST"
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(developer => {
+            let option = document.createElement('option');
+            option.value = developer.id; 
+            option.textContent = developer.name;
+            developerSelect.appendChild(option);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+
+
+    let submitButton = document.createElement('button');
+    submitButton.textContent = "Küldés"
+    submitButton.classList.add("submit-btn");
+    submitButton.addEventListener("click", (event) => {
+        const taskWrapper = event.target.parentNode
+        const data = {
+            "name": taskWrapper.querySelector(".name-input").value.toString(),
+            "deadline": new Date(taskWrapper.querySelector(".time-input").value).toISOString(),
+            "projectID": taskWrapper.parentNode.getAttribute('data-project-id'),
+            "description": taskWrapper.querySelector(".description-input").value.toString(),
+            "developerID": taskWrapper.querySelector(".developer-input").value,
+        }
+        submitTask(data)
+    })
+
+
+
     mainContainer.appendChild(name);
     mainContainer.appendChild(nameInput);
     mainContainer.appendChild(description);
     mainContainer.appendChild(descriptionInput);
+    mainContainer.appendChild(time);
+    mainContainer.appendChild(timeInput);
     mainContainer.appendChild(developer);
     mainContainer.appendChild(developerSelect);
+    mainContainer.appendChild(submitButton);
+
     appendTo.appendChild(mainContainer)
+}
+
+function submitTask(data) {
+    fetch("https://localhost:7295/RedMineDataList/addtask", {
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(developer => {
+                let option = document.createElement('option');
+                option.value = developer.id; 
+                option.textContent = developer.name;
+                developerSelect.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 function loadWithGet(address,helperFunction)
@@ -91,22 +192,32 @@ function startUp()
 startUp();
 
 
-// function filterByType(type, checkbox) {
-//     if (checkbox.checked) {
-//         // Filters by type
-//     } else {
-//         // Removes filter by type
-//     }
-// }
+function filterByType(type, checkbox) {
+    if (checkbox.checked) {
+        document.querySelectorAll(".project-container").forEach((element) => {
+            if (element.getAttribute('data-project-type') !== type.toString()) {
+                element.style.display = "none";
+            }
+        })
+        console.log(type)
+        // Filters by type  
+    } else {
+        document.querySelectorAll(".project-container").forEach((element) => {
+            if (element.getAttribute('data-project-type') !== type.toString()) {
+                element.style.display = "flex";
+            }
+        })
+    }
+}
 
-// function toggleDropdown() {
+function toggleDropdown() {
 
-//     const dropdown = document.getElementById("filter-dropdown");
+    const dropdown = document.getElementById("filter-dropdown");
 
-//     if (dropdown.style.visibility === "hidden") {
-//         dropdown.style.visibility = "visible"
-//     } else {
-//         dropdown.style.visibility = "hidden"
-//     }
+    if (dropdown.style.visibility === "hidden") {
+        dropdown.style.visibility = "visible"
+    } else {
+        dropdown.style.visibility = "hidden"
+    }
 
-// }
+}
